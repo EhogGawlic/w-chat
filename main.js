bddb763f2ae4cd3b104727b615be88d3acd35e0d
EhogGawlic <ethang0227@gmail.com>
2025-09-05 14:51:48 -0700

    Now I fixed it

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ app.get('/', async (req, res) => {
     let contacts = ``
     const posts = await getAllData('posts')
     const tusr = user?await getOneData('users', {username: user.username}):null
-    const cntcts = tusr?tusr.contacts:[]
+    const cntcts = tusr?tusr.contacts:[].toArray()
     console.log(cntcts)
     cntcts.forEach(c => {
         console.log(c)
@@ -233,4 +233,5 @@ app.set('views', __dirname)
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })//
+
 
